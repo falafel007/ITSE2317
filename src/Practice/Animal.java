7864f258297835996dd56b7542192243f3877770
falafel007 <felleisen@gmail.com>
2023-08-28 20:17:32 -0500

    initial upload

--- src/Practice/Animal.java
+++ src/Practice/Animal.java
@@ -1,4 +1,17 @@
 package Practice;
 
 public class Animal {
+
+    public static void main(String[] args) {
+        Animal animal; // assigns animal variable for type animal (or any subclass thereof)
+        // animal = new... instatiates animal variable as specifc object
+    }
+}
+
+class Bird extends Animal {
+
+}
+
+class Frog extends Animal {
+
 }
